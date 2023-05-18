ac4f6fae7e4bec487f2840e8605813a9c91bde52
bzaman_tk <bzaman.tk@gmail.com>
2023-05-18 21:11:30 +0600

    404error page added

--- src/pages/error/Error404.jsx
+++ src/pages/error/Error404.jsx
@@ -1,9 +1,12 @@
-
+import { Link } from 'react-router-dom';
+import img from '../../assets/error.jpg';
 
 const Error404 = () => {
     return (
-        <div>
-            error page
+        <div className='h-screen flex flex-col items-center justify-center'>
+            <img src={img} className='w-1/3 mx-auto mb-10' alt="" />
+            <h2 className="text-5xl text-red-500 mb-9">Page NOT Found</h2>
+            <Link className='text-2xl' to='/'>Go Back to <span className="text-blue-500">Home</span></Link>
         </div>
     );
 };
