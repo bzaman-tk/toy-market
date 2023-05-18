4a1b5a7124831478967131ef2ab80c975cbf76f2
bzaman_tk <bzaman.tk@gmail.com>
2023-05-18 23:16:12 +0600

    single item page done

--- src/pages/singletoy/SingleToy.jsx
+++ src/pages/singletoy/SingleToy.jsx
@@ -3,10 +3,22 @@ import { useLoaderData } from 'react-router-dom';
 
 const SingleToy = () => {
     const loadedData = useLoaderData()
-    console.log(loadedData);
+    const { _id, seller, ratting, price, photo, name, email, details, categorys } = loadedData[0]
+    console.log(loadedData[0].photo);
     return (
-        <div>
-            hi
+        <div className="hero h-[550px] bg-base-200 mt-10">
+            <div className="hero-content flex-col lg:flex-row gap-20">
+                <img src={photo} className="max-w-sm rounded-lg shadow-2xl" />
+                <div className='w-1/2'>
+                    <h1 className="text-5xl font-bold">{name}</h1>
+                    <p className="py-2 mt-10"><strong>Seller Email:</strong> {email}</p>
+                    <p className="py-2"><strong>Seller Name:</strong> {seller}</p>
+                    <p className="py-2"><strong>Price: </strong> {price}</p>
+                    <p className="py-2"><strong>Ratting:</strong> {ratting}</p>
+                    <p className="py-2"><strong>Toy Category:</strong> {categorys}</p>
+                    <p className="py-2"><strong>Description:</strong> {details}</p>
+                </div>
+            </div>
         </div>
     );
 };
