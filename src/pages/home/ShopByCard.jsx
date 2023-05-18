f746ee1adf137496cb802c00eccc7338e767d34e
bzaman_tk <bzaman.tk@gmail.com>
2023-05-18 16:54:18 +0600

    sweet alert and redirect to login page

--- src/pages/home/ShopByCard.jsx
+++ src/pages/home/ShopByCard.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthProvider';
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom';
 
 const ShopByCard = ({ data }) => {
+    const { user } = useContext(AuthContext)
     const { _id, photo, name, ratting, price } = data;
-    console.log(data);
+    const navigate = useNavigate()
+    // console.log(data);
+
+    const handleView = () => {
+        let timerInterval
+        Swal.fire({
+            title: 'You Have To Login First',
+            html: 'Redirect to Login page in <b></b> milliseconds.',
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                }, 100)
+            },
+            willClose: () => {
+                clearInterval(timerInterval)
+            }
+        }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer')
+                navigate('/login')
+            }
+        })
+    }
+
     return (
         <div className="card w-96 bg-gray-900 shadow-xl">
             <figure>
@@ -12,9 +44,13 @@ const ShopByCard = ({ data }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>Price: {price}</p>
                 <p>Ratting: {ratting}</p>
-
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">View Details</button>
+                    {
+                        user ?
+                            <button className="btn btn-primary">View Details</button>
+                            :
+                            <button onClick={handleView} className="btn btn-primary">View Details</button>
+                    }
                 </div>
             </div>
         </div>
