227697f4a964a3c2bc3480cb93d204137986a537
bzaman_tk <bzaman.tk@gmail.com>
2023-05-19 00:38:24 +0600

    delete function implemented

--- src/pages/account/MyToys.jsx
+++ src/pages/account/MyToys.jsx
@@ -1,18 +1,49 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../auth/AuthProvider';
 import MyToysCard from './MyToysCard';
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const { user } = useContext(AuthContext)
     const [toys, setToys] = useState(null)
+    const [reload, setReload] = useState(false)
     useEffect(() => {
         fetch(`http://localhost:5000/my-toys?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setToys(data))
-    }, [user])
+    }, [user, reload])
 
     const handleDelete = id => {
-        console.log(id);
+        // console.log(id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            setReload(!reload)
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+
+
     }
 
     return (
