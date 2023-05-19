4fe1762c36408a97f5f316941bee859087bd958c
bzaman_tk <bzaman.tk@gmail.com>
2023-05-19 15:31:34 +0600

    modal added

--- src/pages/account/MyToysCard.jsx
+++ src/pages/account/MyToysCard.jsx
@@ -16,8 +16,21 @@ const MyToysCard = ({ data, index, handleDelete }) => {
             <td>{quantity}</td>
             <td>{details}</td>
             <td>
-                <button className="btn mr-2">Update</button>
+                <label htmlFor={`my-modal-${index}`} className="btn mr-2">
+                    Update
+                </label>
                 <button onClick={() => handleDelete(_id)} className="btn text-red-500">X</button>
+
+                <input type="checkbox" id={`my-modal-${index}`} className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box w-11/12 max-w-5xl">
+                        <h3 className="font-bold text-lg">Congratulations random Internet user! {`my-modal-${index}`}</h3>
+                        <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div className="modal-action">
+                            <label htmlFor={`my-modal-${index}`} className="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
             </td>
         </tr>
     );
