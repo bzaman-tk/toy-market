c6910cbf51c99220ec42eca69435f2b2ca7243ae
bzaman_tk <bzaman.tk@gmail.com>
2023-05-21 07:38:53 +0600

    blog image updated

--- src/pages/home/OurStory.jsx
+++ src/pages/home/OurStory.jsx
@@ -20,11 +20,11 @@ const OurStory = () => {
                 className="sm:flex justify-center gap-12 mt-12">
                 <div className="card mb-5 sm:mb-0 rounded-none  sm:w-1/4 bg-base-100 shadow-xl border border-gray-900">
                     <figure>
-                        <img className='h-64 w-full' src="https://cdn.shopify.com/s/files/1/0508/1828/5750/products/RF-YQFP-KVQ3-1_540x.jpg?v=1655892871" alt="" />
+                        <img className='h-64 w-full' src="https://images.squarespace-cdn.com/content/v1/5b0c868870e802807f35f826/1656634353473-0E10KJU489X1IO1LQCCN/Screen+Shot+2022-06-30+at+6.09.30+PM.png?format=1500w" alt="" />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title mb-2">NOSTRO EXPE TENDA </h2>
-                        <p>dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <h2 className="card-title mb-2">Star Wars Day</h2>
+                        <p>Its May the 4th, and Star Wars fans around the world are celebrating Star Wars Day! To commemorate this occasion</p>
                         <div className="card-actions justify-end">
                             <button className="btn block mx-auto mt-5">Read More</button>
                         </div>
@@ -32,23 +32,23 @@ const OurStory = () => {
                 </div>
                 <div className="card mb-5 sm:mb-0 rounded-none sm:w-1/4 bg-base-100 shadow-xl border border-gray-900">
                     <div className="card-body">
-                        <h2 className="card-title mb-2">NOSTRO EXPE TENDA </h2>
-                        <p>dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <h2 className="card-title mb-2">The Crow, Marvel</h2>
+                        <p>Its May the 4th, and Star Wars fans around the world are celebrating Star Wars Day! To commemorate this occasion</p>
                         <div className="card-actions justify-end">
                             <button className="btn block mx-auto mt-5">Read More</button>
                         </div>
                     </div>
                     <figure>
-                        <img className='h-64 w-full' src="https://cdn.shopify.com/s/files/1/0508/1828/5750/products/RF-YQFP-KVQ3-1_540x.jpg?v=1655892871" alt="" />
+                        <img className='h-64 w-full' src="https://bostonglobe-prod.cdn.arcpublishing.com/resizer/14Y6sOtXbFqFoGj0XRMcprM0lzQ=/1024x0/cloudfront-us-east-1.images.arcpublishing.com/bostonglobe/XCXJQOUI2JPAU4XUDUJQ6PQZRQ.jpg" alt="" />
                     </figure>
                 </div>
                 <div className="card mb-5 sm:mb-0 rounded-none sm:w-1/4 bg-base-100 shadow-xl border border-gray-900">
                     <figure>
-                        <img className='h-64 w-full' src="https://cdn.shopify.com/s/files/1/0508/1828/5750/products/RF-YQFP-KVQ3-1_540x.jpg?v=1655892871" alt="" />
+                        <img className='h-64 w-full' src="https://akns-images.eonline.com/eol_images/Entire_Site/201363/rs_1024x759-130703124141-1024.Gru.mh.070313.jpg" alt="" />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title mb-2">NOSTRO EXPE TENDA </h2>
-                        <p>dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <h2 className="card-title mb-2">Celebtes 85th Anversary</h2>
+                        <p>Its May the 4th, and Star Wars fans around the world are celebrating Star Wars Day! To commemorate this occasion</p>
                         <div className="card-actions justify-end">
                             <button className="btn block mx-auto mt-5">Read More</button>
                         </div>
